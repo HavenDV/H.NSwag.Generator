15162a3e9d36f6d5d1902b3dd3e522fdbc5a2921
Konstantin S <HavenDV@users.noreply.github.com>
2021-06-14 22:56:41 +1000

    test: Added Execute test.

--- src/tests/H.NSwag.Generator.IntegrationTests/NSwagGeneratorTests.cs
+++ src/tests/H.NSwag.Generator.IntegrationTests/NSwagGeneratorTests.cs
@@ -1,5 +1,12 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace H.NSwag.Generator.IntegrationTests
@@ -10,7 +17,7 @@ namespace H.NSwag.Generator.IntegrationTests
         [TestMethod]
         public void GenerateTest()
         {
-            var text = H.Resources.openapi1;
+            var text = Resources.openapi1;
             var path = Path.GetTempFileName();
             File.WriteAllText(path, text);
 
@@ -34,7 +41,7 @@ namespace H.NSwag.Generator.IntegrationTests
         {
             var exception = Assert.ThrowsException<InvalidOperationException>(() =>
             {
-                var text = H.Resources.openapi2;
+                var text = Resources.openapi2;
                 var path = Path.GetTempFileName();
                 File.WriteAllText(path, text);
 
@@ -46,12 +53,78 @@ namespace H.NSwag.Generator.IntegrationTests
                     Directory.SetCurrentDirectory(Path.GetDirectoryName(path) ?? string.Empty);
                 }
 
-                var source = NSwagGenerator.Generate(
+                var _ = NSwagGenerator.Generate(
                     consolePath,
                     path);
             });
 
             Console.WriteLine(exception);
+        }
+
+        [TestMethod]
+        public void ExecuteTest()
+        {
+            var text = Resources.openapi1;
+            var path = Path.GetTempFileName() + ".nswag";
+            File.WriteAllText(path, text);
+
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                Directory.SetCurrentDirectory(Path.GetDirectoryName(path) ?? string.Empty);
+            }
+
+            var inputCompilation = CSharpCompilation.Create(
+                "compilation",
+                new[] { CSharpSyntaxTree.ParseText(@"
+namespace MyCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+") },
+                new[]
+                {
+                    MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location),
+                },
+                new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+
+            var generator = new NSwagGenerator();
+            var driver = (GeneratorDriver)CSharpGeneratorDriver.Create(
+                new ISourceGenerator[] { generator },
+                new AdditionalText[] { new CustomAdditionalText(path) });
+
+            driver.RunGeneratorsAndUpdateCompilation(
+                inputCompilation, 
+                out var outputCompilation, 
+                out var diagnostics);
+
+            diagnostics.Should().BeEmpty();
+            outputCompilation.SyntaxTrees.Should().HaveCount(2);
+
+            var source = outputCompilation.SyntaxTrees.ElementAt(1).GetText().ToString();
+            Console.WriteLine(source);
+        }
+    }
+
+    public class CustomAdditionalText : AdditionalText
+    {
+        public string Text { get; }
+
+        public override string Path { get; }
+
+        public CustomAdditionalText(string path)
+        {
+            Path = path;
+            Text = File.ReadAllText(path);
+        }
+
+        public override SourceText GetText(CancellationToken cancellationToken = default)
+        {
+            return SourceText.From(Text);
         }
     }
 }
